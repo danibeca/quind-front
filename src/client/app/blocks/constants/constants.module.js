a65d081e2e2356ae5abbc577bffc2af80c395554
danibeca <danibeca@okazo.co>
2017-03-30 00:28:42 -0500

    Updating local api ip.

--- src/client/app/blocks/constants/constants.module.js
+++ src/client/app/blocks/constants/constants.module.js
@@ -1,4 +1,4 @@
 /* jshint ignore:start */
 // jscs:disable
 angular.module("blocks.constants", [])
-.constant("environmentConfig", {"api":"http://localhost/laravel/agilin/api","env":"dev"});
+.constant("environmentConfig", {"api":"http://192.168.0.12/laravel/agilin/api","env":"dev"});
