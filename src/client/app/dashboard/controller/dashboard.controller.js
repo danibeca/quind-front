6799b4ba0ca8ff7dd2124ecb7cc81b299fd3640a
Johan Ruiz <jruiz6@gmail.com>
2017-11-07 21:39:01 -0500

    Reduce load time for Dashboard page

--- src/client/app/dashboard/controller/dashboard.controller.js
+++ src/client/app/dashboard/controller/dashboard.controller.js
@@ -8,11 +8,15 @@
         .controller('DashboardController', DashboardController);
 
     /* @ngInject */
-    function DashboardController(userService, componentService, ciServerService, $filter, storageService, $timeout) {
+    function DashboardController(userService, componentService, ciServerService, $filter, storageService, $timeout, $scope) {
         var vm = this;
         var croot = storageService.getJsonObject('croot');
 
         vm.user = userService.getUser();
+        vm.data = [];
+        vm.dataSeries = [];
+        vm.ciData = [];
+        vm.ciDataSeries = [];
 
         vm.hasCIS = false;
         vm.chartId = 'dashChart1';
@@ -30,6 +34,7 @@
             loadComponentsInfo();
             loadQAAttributes();
             loadQAIndicators();
+            loadQAIndicatorSeries();
         }
 
         function loadCIServerInstances() {
@@ -42,6 +47,7 @@
                 if (data.length > 0) {
                     vm.hasCIS = true;
                     loadCIIndicators();
+                    loadCIIndicatorSeries();
                 }
             }
 
@@ -82,6 +88,159 @@
             }
         }
 
+        function loadCIIndicators() {
+            if (vm.hasCIS) {
+                componentService.getCIIndicators(croot.id, '1')
+                    .then(successCIIndicators)
+                    .catch(failCIIndicators);
+            }
+
+            $scope.$watch('vm.ciData', function () {
+                if (isCIInfoReady()) {
+                    prepareCISeriesIndicators();
+                }
+            });
+
+            function successCIIndicators(indicators) {
+                vm.ciData = indicators;
+            }
+
+            function failCIIndicators(error) {
+                console.log(error);
+                vm.ciIndicatorsError = true;
+                vm.ciSeriesError = true;
+            }
+        }
+
+        function loadCIIndicatorSeries() {
+            componentService.getCIIndicatorSeries(croot.id, '1')
+                .then(successCISeries)
+                .catch(failCISeries);
+
+            $scope.$watch('vm.ciDataSeries', function () {
+                if (isCIInfoReady()) {
+                    prepareCISeriesIndicators();
+                }
+            });
+
+            function successCISeries(indi) {
+                vm.ciDataSeries = indi;
+            }
+
+            function failCISeries(error) {
+                console.log(error);
+                vm.ciSeriesError = true;
+            }
+        }
+
+        function loadQAIndicators() {
+            componentService.getQAIndicators(croot.id, vm.indIds)
+                .then(successQAIndicators)
+                .catch(failQAIndicators);
+
+            $scope.$watch('vm.data', function () {
+                if (isQAInfoReady()) {
+                    prepareQASeriesIndicators();
+                }
+            });
+
+            function successQAIndicators(indicators) {
+                vm.data = indicators;
+            }
+
+            function failQAIndicators(error) {
+                vm.error = true;
+                vm.seriesError = true;
+                vm.msgError = error.msgCode;
+            }
+        }
+
+        function loadQAIndicatorSeries() {
+            componentService.getQAIndicatorSeries(croot.id, vm.indIds)
+                .then(successSeries);
+
+            $scope.$watch('vm.dataSeries', function () {
+                if (isQAInfoReady()) {
+                    prepareQASeriesIndicators();
+                }
+            });
+
+            function successSeries(indi) {
+                vm.dataSeries = indi;
+            }
+        }
+
+        function isQAInfoReady() {
+            if (vm.data.length > 0 && vm.dataSeries.length > 0) {
+                return true;
+            }
+        }
+
+        function isCIInfoReady() {
+            if (vm.ciData.length > 0 && vm.ciDataSeries.length > 0) {
+                return true;
+            }
+        }
+
+        function prepareQASeriesIndicators() {
+            var missing = vm.data.length;
+            var ids = [];
+            var labels = [];
+            var dSeries = [];
+            vm.data.forEach(function (name) {
+                vm.dataSeries.forEach(function (indicator) {
+                    if (indicator[name.id] !== undefined) {
+                        missing--;
+                        ids.push(name.id);
+                        labels[name.id] = {
+                            'title': name.name
+                        };
+                        dSeries[name.id] = indicator[name.id];
+
+                        if (missing === 0) {
+                            createRunChart();
+                        }
+                    }
+                });
+            });
+
+            function createRunChart() {
+                vm.ids = ids;
+                vm.labels = labels;
+                vm.series = dSeries;
+            }
+        }
+
+        function prepareCISeriesIndicators() {
+            var missing = vm.ciData.length;
+            var ids = [];
+            var labels = [];
+            var dSeries = [];
+
+            vm.ciData.forEach(function (name) {
+                vm.ciDataSeries.forEach(function (indicator) {
+                    if (indicator[name.id] !== undefined) {
+                        missing--;
+                        ids.push(name.id);
+                        labels[name.id] = {
+                            'title': name.name
+                        };
+                        dSeries[name.id] = indicator[name.id];
+
+                        if (missing === 0) {
+                            createRunChart();
+                        }
+                    }
+                });
+            });
+
+            function createRunChart() {
+                vm.ciIds = ids;
+                vm.ciLabels = labels;
+                vm.ciSeries = dSeries;
+            }
+        }
+
         function buildAxe(title) {
             var labelsArray = [[0, ''],
                 [1, $filter('translate')('TOO_LOW_TEXT')],
@@ -108,118 +267,6 @@
             vm.qaConfig.bulletBorderAlpha = 1;
             vm.qaConfig.bulletBorderThickness = 2;
             vm.qaConfig.bulletAlpha = 0.8;
-        }
-
-        function loadQAIndicators() {
-            componentService.getQAIndicators(croot.id, vm.indIds)
-                .then(successQAIndicators)
-                .catch(failQAIndicators);
-
-            function successQAIndicators(indicators) {
-                vm.data = indicators;
-                loadQAIndicatorSeries(indicators);
-            }
-
-            function failQAIndicators(error) {
-                vm.error = true;
-                vm.seriesError = true;
-                vm.msgError = error.msgCode;
-            }
-        }
-
-        function loadCIIndicators() {
-            if (vm.hasCIS) {
-                componentService.getCIIndicators(croot.id, '1')
-                    .then(successCIIndicators)
-                    .catch(failCIIndicators);
-            }
-
-            function successCIIndicators(indicators) {
-                vm.ciData = indicators;
-                loadCIIndicatorSeries(indicators);
-            }
-
-            function failCIIndicators(error) {
-                console.log(error);
-                vm.ciIndicatorsError = true;
-                vm.ciSeriesError = true;
-            }
-        }
-
-        function loadQAIndicatorSeries(indicators) {
-            var missing = indicators.length;
-            var ids = [];
-            var labels = [];
-            var dSeries = [];
-
-            componentService.getQAIndicatorSeries(croot.id, vm.indIds)
-                .then(successSeries);
-
-            function successSeries(indi) {
-                indicators.forEach(function (name) {
-                    indi.forEach(function (indicator) {
-                        if (indicator[name.id] !== undefined) {
-                            missing--;
-                            ids.push(name.id);
-                            labels[name.id] = {
-                                'title': name.name
-                            };
-                            dSeries[name.id] = indicator[name.id];
-
-                            if (missing === 0) {
-                                createRunChart();
-                            }
-                        }
-                    });
-                });
-            }
-
-            function createRunChart() {
-                vm.ids = ids;
-                vm.labels = labels;
-                vm.series = dSeries;
-            }
-        }
-
-        function loadCIIndicatorSeries(indicators) {
-            var missing = indicators.length;
-            var ids = [];
-            var labels = [];
-            var dSeries = [];
-
-            componentService.getCIIndicatorSeries(croot.id, '1')
-                .then(successCISeries)
-                .catch(failCISeries);
-
-            function successCISeries(indi) {
-                indicators.forEach(function (name) {
-                    indi.forEach(function (indicator) {
-                        if (indicator[name.id] !== undefined) {
-                            missing--;
-                            ids.push(name.id);
-                            labels[name.id] = {
-                                'title': name.name
-                            };
-                            dSeries[name.id] = indicator[name.id];
-
-                            if (missing === 0) {
-                                createRunChart();
-                            }
-                        }
-                    });
-                });
-            }
-
-            function failCISeries(error) {
-                console.log(error);
-                vm.ciSeriesError = true;
-            }
-
-            function createRunChart() {
-                vm.ciIds = ids;
-                vm.ciLabels = labels;
-                vm.ciSeries = dSeries;
-            }
         }
     }
 })();
